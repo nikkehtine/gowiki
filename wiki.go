2997cc644e4d1273e9cdfc84dc22e8a4bb118501
Nikkeh <nikkehtine@int.pl>
2023-09-05 13:34:02 +0200

    Add page struct, functions and methods to save a page and to load a page

--- wiki.go
+++ wiki.go
@@ -1,3 +1,26 @@
 package main
 
+import "os"
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+// Saves the page to a file
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+// Loads a page with the given title
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{title, body}, nil
+}
+
 func main() {}
